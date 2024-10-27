fb2491b1076584aa4e2532857633f44595c93aa1
Cristian Andres <cristianvcx188@gmail.com>
2024-10-27 15:54:11 -0300

    actualizado

---
+++ components/StatusBadge.tsx
@@ -0,0 +1,29 @@
+import { StatusIcon } from "@/constants";
+import clsx from "clsx";
+import Image from "next/image";
+import React from "react";
+
+const StatusBadge = ({ status }: { status: Status }) => {
+  return ( 
+    <div className={clsx('status-badge', {
+        'bg-green-600': status === 'programado',
+        'bg-blue-600': status === 'pendiente',
+        'bg-red-600': status === 'cancelado',
+    })}>
+        <Image 
+            src={StatusIcon[status]}
+            alt={status}
+            width={24}
+            height={24}
+            className="h-fit w-3"
+        />
+        <p className={clsx('text-12-semibold capitalize', {
+            'text-green-500': status === 'programado',
+            'text-blue-500': status === 'pendiente',
+            'text-red-500': status === 'cancelado',
+        })}>{status}</p>
+    </div>
+  )
+};
+
+export default StatusBadge;
